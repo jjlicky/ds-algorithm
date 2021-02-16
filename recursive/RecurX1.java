e11b30a734f9f2da0f4a35c576207f5d9c38607f
jjlicky <jjlicky@yonsei.ac.kr>
2021-02-16 22:52:11 +0900

    recursive

---
+++ recursive/RecurX1.java
@@ -0,0 +1,25 @@
+package recursive;
+import java.util.Scanner;
+
+class RecurX1 {
+	
+	static void recur(int n) {
+		IntStack s = new IntStack(n);
+		
+		while(true) {
+			if(n>0) {
+				s.push(n);
+				n=n-1;
+				continue;
+			}
+			
+			if(s.isEmpty() != true) {
+				n=s.pop();
+				System.out.println(n);
+				n=n-2;
+				continue;
+			}
+			break;
+		}
+	}
+}
